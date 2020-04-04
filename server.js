54c30d4bb0fb27920f1816d6022148f9180bab7b
Welton L. Santos <welton.santos.bcc@gmail.com>
2020-04-04 01:28:46 -0300

    rota de store

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const requiredDir = require ('require-dir');
 
 //iniciando o App
 const app = express('');
+//aceita método post via json
+app.use(express.json());
 
 //iniciando DB
 mongoose.connect("mongodb://localhost:27017/nodeapi",{
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,9 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController') 
 
-//primeira rota
+//rotas
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.store);
 
 module.exports = routes;
   
